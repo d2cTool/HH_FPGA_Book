8a2b7f0e0dcc7d7a870dff3cba42b8cb6ed5b438
d2c.tool@gmail.com <R000000t>
2019-01-27 20:51:24 +0300

    yosys not supported system verilog syntax

--- Book examples/Example 4.1/design.sv
+++ Book examples/Example 4.1/design.sv
@@ -1,10 +1,10 @@
 // Design
 // Example 4.1
 module sillyfunction (a, b, c, y);
-  input logic      a;
-  input logic      b;
-  input logic      c;
-  output logic     y;
+  input       a;
+  input       b;
+  input       c;
+  output      y;
 
   assign y = ~a & ~b & ~c |
     		  a & ~b & ~c |
